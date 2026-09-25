2e5f21d85f995f0ee74a015ae7cadaef0d8a4537
hibaalshalabi <hibaalshalabi1@gmail.com>
2026-09-25 10:27:23 +0300

    Enhance data analysis with additional statistics
    Added calculations for standard deviation, min, and max values.

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-""" import numpy as np
-
-data = np.genfromtxt('data.csv', delimiter=',', skip_header=1, usecols=range(0, 7))
-
-print("Shape:", data.shape)
-print("\nFirst 5 rows:\n", data[:5])
-print("\nMissing values count:", np.isnan(data).sum())
-print("\nMean of each column:\n", np.nanmean(data, axis=0))
-
-"""""
-
-
 import numpy as np
 import os
 
@@ -22,4 +10,8 @@ data = np.genfromtxt(file_path, delimiter=',', skip_header=1, usecols=range(0, 7
 print("Shape:", data.shape)
 print("\nFirst 5 rows:\n", data[:5])
 print("\nMissing values count:", np.isnan(data).sum())
-print("\nMean of each column:\n", np.nanmean(data, axis=0))
+
+print("Mean:", np.nanmean(data, axis=0))
+print("Std:", np.nanstd(data, axis=0))
+print("Min:", np.nanmin(data, axis=0))
+print("Max:", np.nanmax(data, axis=0))
